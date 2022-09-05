20481a91a776ffeef01df0b7b0e10484a1f6d497
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2022-09-05 15:47:39 -0300

    Add test which expects an exception

--- my-source-code/node-testes/carrinho/test/carrinho.test.js
+++ my-source-code/node-testes/carrinho/test/carrinho.test.js
@@ -28,4 +28,13 @@ describe('Testes do carrinho', () => {
 
     expect(carrinho).toHaveProperty('total');
   });
+
+  it('Deve lançar erro ao finalizar compra com carrinho vazio', () => {
+    function englobaErroCarrinho() {
+      const carrinho = new Carrinho();
+      carrinho.finalizaCompra();
+    }
+
+    expect(englobaErroCarrinho).toThrowError('Carrinho de compras vazio');
+  });
 });
